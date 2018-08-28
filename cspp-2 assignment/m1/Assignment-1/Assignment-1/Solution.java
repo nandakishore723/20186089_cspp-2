cc5748d943bf91669cb47b2db023a770f55d9699
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-28 22:10:53 +0530

    namennum

---
+++ cspp-2 assignment/m1/Assignment-1/Assignment-1/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		String name = ip.nextLine();
+		long rollno = ip.nextLong();
+		System.out.println("Name :"+ name);
+		System.out.println("Rollno :"+ rollno);
+	}
+}
