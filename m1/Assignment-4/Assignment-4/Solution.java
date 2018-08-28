6320bef31cd895084adfa868410f19be70c84655
nandakishore723 <nandakishore723@msitprogram.net>
2018-08-28 13:40:49 +0530

    degreestof

--- m1/Assignment-4/Assignment-4/Solution.java
+++ m1/Assignment-4/Assignment-4/Solution.java
@@ -7,10 +7,13 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		double degreesCelsius = scan.nextDouble();
-		calculateFromDegreesToFarenheit(degreesCelsius);
+		System.out.println(calculateFromDegreesToFarenheit(degreesCelsius));
 	}
 	/*
 	Need to fill the calculateFromDegreesToFarenheit function and print the output
 	of fahrenheit.
 	*/
+	public static double calculateFromDegreesToFarenheit(double degreesCelsius){
+		return degreesCelsius*1.8 + 32;
+	}
 }
