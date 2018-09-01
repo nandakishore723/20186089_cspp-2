2cabf824d7113898b4ea8a3a6fb9a23fda1559e5
nandakishore723 <nandakishore723@msitprogram.net>
2018-09-01 16:40:34 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp-2 assignment/m3/Assignment-2/Assignment-2/Solution.java
@@ -0,0 +1,57 @@
+/**
+ *@author : nandakishore723.
+ * { item_description }.
+ */
+import java.util.Scanner;
+/**
+ * Do not modify this main function.
+ */
+public final class Solution {
+    /**
+     * { global variable }.
+     */
+    public static final int N_VALUE = 10;
+    /**
+     * { global variable }.
+     */
+    public static final int N_VALUE1 = 7;
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    //we are not using this constructor.
+    }
+/**
+ * Fill the main function to.
+ * print the number of 7's between 1 to n.
+ * @param      args  The arguments.
+ */
+    public static void main(final String[] args) {
+        Scanner ip = new Scanner(System.in);
+        int n = ip.nextInt();
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            count += numbers(i);
+        }
+        System.out.println(count);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      n     { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    public static int numbers(final int n) {
+        int count = 0;
+        int va = n;
+        while (va > 0) {
+            if (va % N_VALUE == N_VALUE1) {
+                count = count + 1;
+            }
+            va = va / N_VALUE;
+        }
+        return count;
+    }
+}
+
