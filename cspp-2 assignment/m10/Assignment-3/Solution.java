c7b35be39c8324af436e8200ce3eb36d0e5f2f0a
nandakishore723 <nandakishore723@msitprogram.net>
2018-09-06 18:33:43 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp-2 assignment/m10/Assignment-3/Solution.java
+++ cspp-2 assignment/m10/Assignment-3/Solution.java
@@ -67,6 +67,9 @@ public class Solution {
      *
      * @param      args  The arguments
      */
+    private Solution() {
+
+    }
     public static void main(final String[] args) {
     // create an object of the list to invoke methods on it
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
