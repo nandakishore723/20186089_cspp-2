cac09610673383c98e8a20f17587f2786d84543a
nandakishore723 <nandakishore723@msitprogram.net>
2018-09-06 16:51:57 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp-2 assignment/m10/Assignment-1/List.java
+++ cspp-2 assignment/m10/Assignment-1/List.java
@@ -418,9 +418,10 @@ public class List {
                         l.add(Integer.parseInt(tokens[1]));
                     }
                     else {
-                        if (t.length > 1)
+                        if (t.length > 1) {
                             l.add(Integer.parseInt(t[0]),
                             Integer.parseInt(t[1]));
+                        }
                     }
                 }
                 break;
