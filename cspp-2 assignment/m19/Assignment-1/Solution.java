1d99414aecce2123503fd2baf15cdd7b851ae45d
nandakishore723 <nandakishore723@msitprogram.net>
2018-09-15 12:21:20 +0530

    m19

---
+++ cspp-2 assignment/m19/Assignment-1/Solution.java
@@ -0,0 +1,129 @@
+import java.util.Scanner;
+
+/**
+ * Solution class for code-eval.
+ */
+public final class Solution {
+     /**
+      * Constructs the object.
+      */
+    private Solution() {
+        // leave this blank
+    }
+    class Quiz {
+    	private String question;
+    	private String[] choice;
+    	private String correctanswer;
+    	private String marks;
+    	private String penalty;
+    	Quiz(String question, String[] choice, String correctanswer, String marks, String penalty){
+    		this.question = question;
+    		this.choice = choice;
+    		this.correctanswer = correctanswer;
+    		this.marks = marks;
+    		this.penalty = penalty;
+    	}
+    }
+    /**
+     * main function to execute test cases.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+         // instantiate this Quiz
+        Quiz q = new Quiz();
+         // code to read the test cases input file
+        Scanner s = new Scanner(System.in);
+        // check if there is one more line to process
+        while (s.hasNext()) {
+            // read the line
+            String line = s.nextLine();
+             // split the line using space
+            String[] tokens = line.split(" ");
+              // based on the list operation invoke the corresponding method
+            switch (tokens[0]) {
+                case "LOAD_QUESTIONS":
+                System.out.println("|----------------|");
+                System.out.println("| Load Questions |");
+                System.out.println("|----------------|");
+                loadQuestions(s, q, Integer.parseInt(tokens[1]));
+                break;
+                case "START_QUIZ":
+                System.out.println("|------------|");
+                System.out.println("| Start Quiz |");
+                System.out.println("|------------|");
+                startQuiz(s, q, Integer.parseInt(tokens[1]));
+                break;
+                case "SCORE_REPORT":
+                System.out.println("|--------------|");
+                System.out.println("| Score Report |");
+                System.out.println("|--------------|");
+                displayScore(q);
+                break;
+                default:
+                break;
+            }
+        }
+    }
+    /**
+     * Loads questions.
+     *
+     * @param      s              The scanner object for user input
+     * @param      quiz           The quiz object
+     * @param      questionCount  The question count
+     */
+    public static void loadQuestions(final Scanner s, final Quiz quiz, final int questionCount) {
+        // write your code here to read the questions from the console
+        // tokenize the question line and create the question object
+        // add the question objects to the quiz class
+        if (questionCount == 0) {
+        	System.out.println("Quiz does not have questions");
+        } else {
+        	for (int i = 0; i < questionCount; i++) {
+        		question = s.nextLine();
+        		String[] tokens = line.split(":");
+        		if (tokens[i] !=5) {
+        			System.out.println("Error! Malformed question");
+        		}
+        		if (tokens[1] != 2) {
+        			System.out.println("<question text> does not have enough answer choices");
+        		}
+        		if (tokens[2] > indexOf(choice)) {
+        			System.out.println(
+        				"Error! Correct answer choice number is out of range for <question text>");
+        		}
+        		if (tokens[3] > 0) {
+        			System.out.println("Invalid max marks for <question text>");
+        		}
+        		if (tokens[4] <= 0) {
+        			System.out.println("Invalid penalty for <question text>");
+        		}
+        		}
+        	}
+        }
+    }
+
+
+
+    /**
+     * Starts a quiz.
+     *
+     * @param      s            The scanner object for user input
+     * @param      quiz         The quiz object
+     * @param      answerCount  The answer count
+     */
+    public static void startQuiz(final Scanner s, final Quiz quiz, final int answerCount) {
+        // write your code here to display the quiz questions
+        // read the user responses from the console
+        // store the user respones in the quiz object
+    }
+
+    /**
+     * Displays the score report
+     *
+     * @param      quiz     The quiz object
+     */
+    public static void displayScore(final Quiz quiz) {
+        // write your code here to display the score report
+    }
+}
